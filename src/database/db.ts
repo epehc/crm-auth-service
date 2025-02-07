1bce99b593c0927ee6c58f76283325b1fa1d0fba
Jose Cardona <jpcardonamena@gmail.com>
2025-02-07 15:54:10 +0100

    enabled logging for troubleshooting

--- src/database/db.ts
+++ src/database/db.ts
@@ -6,10 +6,12 @@ dotenv.config()
 const sequelize = new Sequelize(
     process.env.DB_NAME!,
     process.env.DB_USERNAME!,
-    process.env.DB_PASSWORD!, {
+    process.env.DB_PASSWORD!,
+    {
     host: process.env.DB_HOST!,
     dialect: "postgres",
-    logging: false, // Disable SQL logs in the console
-});
+    logging: true, // Disable SQL logs in the console
+    }
+);
 
 export default sequelize;
