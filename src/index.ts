017c9e41c870ba4475a882f384091aa5f17811c8
Jose Cardona <jpcardonamena@gmail.com>
2025-02-07 15:55:56 +0100

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+/// <reference path="../node_modules/@epehc/sharedutilities/types/express.d.ts" />
+
 import express from "express";
 import * as bodyParser from "body-parser";
 import passport from "./middlewares/passport";
@@ -5,9 +7,16 @@ import authRoutes from "./routes/authRoutes";
 import sequelize from "./database/db";
 import {validateEnv} from "./utils/validateEnv";
 import {setupSwagger} from "./utils/swagger";
+import cors from "cors";
 
 const app = express();
 
+const corsOptions = {
+    origin: process.env.FRONTEND_URL, // Your frontend's URL
+    credentials: true, // Allow credentials (cookies, headers)
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 validateEnv();
 setupSwagger(app);
