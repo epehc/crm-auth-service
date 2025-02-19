9d36c7224f2f223cd275bf4003ab1b01dc6df53f
Jose Cardona <jpcardonamena@gmail.com>
2025-02-19 20:21:29 +0100

    made changes to account for deployment

--- src/index.ts
+++ src/index.ts
@@ -23,13 +23,20 @@ setupSwagger(app);
 app.use(passport.initialize());
 app.use('/auth', authRoutes)
 
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT || 8080;
 
+const server = app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+// Connect to the database in the background
 sequelize
     .sync()
     .then(() => {
         console.log("Database connected!");
-        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
     })
-    .catch((err) => console.error("Database connection failed:", err));
+    .catch((err) => {
+        console.error("Failed to connect to database:", err);
+        server.close(); // Stop the server if the DB fails
+    });
 
